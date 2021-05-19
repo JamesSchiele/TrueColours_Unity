e97c9e2140614821374db78c935bed50df286626
James Schiele <jamesschiele@hotmail.com>
2021-05-19 19:47:56 +0100

    rand colour to debug log from enemySpawner

--- GameMechanics/EnemySpawner.cs
+++ GameMechanics/EnemySpawner.cs
@@ -7,9 +7,28 @@ public class EnemySpawner : MonoBehaviour
 
     public GameObject enemy;
 
+    List<string> listOfTypes = new List<string>()
+    {
+        "Red",
+        "Blue",
+        "Yellow"
+    };
+
+    // List<enemyColour> listOfEnemyTypes = new List<enemyColour>()
+    // {
+    //     new enemyColour() { Type = "Red"},
+    //     new enemyColour() { Type = "Blue"},
+    //     new enemyColour() { Type = "Yellow"}
+    // };
+
     // Update is called once per frame
+
     void Start()
     {
         enemy = GameObject.Instantiate(enemy) as GameObject;
+
+        int randInt = Random.Range(1,listOfTypes.Count);
+
+        Debug.Log(listOfTypes[randInt]);
     }
 }
